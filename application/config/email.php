e1f1d4666a695c07adebc399eec66c57b9082f01
Nicholas Valbusa <info@squallstar.it>
2011-12-19 11:55:12 +0100

    better style

--- application/config/email.php
+++ application/config/email.php
@@ -1,7 +1,16 @@
 <?php
-$config['mailtype'] = 'html';
+
+/*
+ * The values below are used for the internal mailer of We Love Difference.
+ */
+
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'mail.yourhost.com';
 $config['smtp_user'] = 'username';
 $config['smtp_pass'] = 'password';
+
+//If you wish you can also configure the User-Agent. (Default is "CI")
 //$config['useragent'] = 'html';
+
+//Please don't change this
+$config['mailtype'] = 'html';
