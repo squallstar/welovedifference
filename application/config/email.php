32a4daf6d907206bcd2efa731fc446262902e3ab
Nicholas Valbusa <info@squallstar.it>
2011-12-19 11:41:25 +0100

    default values on email config

--- application/config/email.php
+++ application/config/email.php
@@ -1,7 +1,7 @@
 <?php
 $config['mailtype'] = 'html';
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'mail.welovedifference.com';
-$config['smtp_user'] = 'info@welovedifference.com';
-$config['smtp_pass'] = 'cojone';
+$config['smtp_host'] = 'mail.yourhost.com';
+$config['smtp_user'] = 'username';
+$config['smtp_pass'] = 'password';
 //$config['useragent'] = 'html';
